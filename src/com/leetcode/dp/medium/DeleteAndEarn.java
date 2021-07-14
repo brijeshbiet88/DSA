ace8ca0de7a39d685d184fe3caed8485cda8e8be
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-07-14 12:06:46 +0530

    Delete and earn 2. Leetcode 740

--- src/com/leetcode/dp/medium/DeleteAndEarn.java
+++ src/com/leetcode/dp/medium/DeleteAndEarn.java
@@ -5,7 +5,7 @@ public class DeleteAndEarn {
 	public static void main(String[] args) {
 		System.out.println("-----------------Test Case 1-------------------");
 		int [] nums = {2 , 2 , 3 , 3, 3 ,4};
-		System.out.println("Max Score = "+deleteAndEarn(nums));
+		System.out.println("Max Score = "+deleteAndEarn2(nums));
 		System.out.println();
 	}
 
@@ -26,5 +26,30 @@ public class DeleteAndEarn {
 	            prev = k;
 	        }
 	        return Math.max(avoid, using);
+	}
+	
+	public static int deleteAndEarn2(int[] nums) {
+			if(nums.length == 0) return 0;
+			
+			if(nums.length == 1) return nums[0];
+			
+			int [] count = new int[10001];
+	        int [] sum = new int [10001];
+	        
+	        int maxValue = 0;
+	        for(int num : nums) {
+	        	count[num]++;
+	        	if(num > maxValue)
+	        		maxValue = num;
+	        }
+	        
+	        sum[0] = count[0];
+	        sum[1] = count[1];
+	        
+	        for(int i = 2 ; i <= maxValue ; i++) {
+	        	sum[i] = Math.max(sum[i-2] + count[i]*i, sum[i-1]);
+	        }
+	        
+			return sum[maxValue];
 	}
 }
