2b7788ae2f715426c4ace4d3d38f52d1097718ae
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-07-18 19:08:02 +0530

    Add String Update. Leetcode 415

--- src/com/leetcode/string/easy/StringAddition.java
+++ src/com/leetcode/string/easy/StringAddition.java
@@ -17,25 +17,25 @@ public class StringAddition {
 	}
 	
 	  public static String addStrings(String num1, String num2) {
-	        StringBuilder res = new StringBuilder();
+	        StringBuilder addition = new StringBuilder();
 
 	        int carry = 0;
-	        int p1 = num1.length() - 1;
-	        int p2 = num2.length() - 1;
-	        while (p1 >= 0 || p2 >= 0) {
-	            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
-	            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+	        int digit1 = num1.length() - 1;
+	        int digit2 = num2.length() - 1;
+	        while (digit1 >= 0 || digit2 >= 0) {
+	            int x1 = digit1 >= 0 ? num1.charAt(digit1) - '0' : 0;
+	            int x2 = digit2 >= 0 ? num2.charAt(digit2) - '0' : 0;
 	            int value = (x1 + x2 + carry) % 10;
 	            carry = (x1 + x2 + carry) / 10;
-	            res.append(value);
-	            p1--;
-	            p2--;    
+	            addition.append(value);
+	            digit1--;
+	            digit2--;    
 	        }
 	        
 	        if (carry != 0)
-	            res.append(carry);
+	            addition.append(carry);
 	        
-	        return res.reverse().toString();
+	        return addition.reverse().toString();
 	    }
 
 }
