9531304935cb62e642c077cd0281ba18ab175f0c
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-07-20 23:56:06 +0530

    Frog Jump Using BFS. Leetcode 403

--- src/com/leetcode/dp/hard/FrogJump.java
+++ src/com/leetcode/dp/hard/FrogJump.java
@@ -1,16 +1,23 @@
 package com.leetcode.dp.hard;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 public class FrogJump {
 
 	public static void main(String[] args) {
+		System.out.println("--------------Test Case 1------------------");
 		int [] stones = {0,1,3,5,6,8,12,17};
 		System.out.println("can Cross ? "+ canCross(stones) );
+		System.out.println();
+		
+		System.out.println("--------------Test Case 2------------------");
+		int [] stones2 = {0,1,3,5,6,8,12,17};
+		System.out.println("can Cross ? "+ canCrossDfs(stones2) );
+		System.out.println();
 	}
 
 	public static boolean canCross(int[] stones) {
@@ -39,6 +46,43 @@ public class FrogJump {
 				if(set.contains(currentPos)) {
 					Pair newPair = new Pair(i, currentPos);
 					queue.add(newPair);
+				}
+			}
+
+		}
+
+		return false;
+
+	}
+	
+	
+	
+	public static boolean canCrossDfs(int[] stones) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int stone : stones)
+			set.add(stone);
+		Stack<Pair> stack = new Stack<Pair>();
+		Pair pair = new Pair(0, 0);
+		stack.add(pair);
+
+		while (!stack.isEmpty()) {
+
+			Pair p = stack.pop();
+			int jump = p.jump;
+			int pos = p.pos;
+
+			if (p.pos == stones[stones.length - 1]) {
+				return true;
+			}
+
+			for (int i = jump - 1; i <= jump + 1; i++) {
+				if (i <= 0) {
+					continue;
+				}
+				int currentPos = pos + i;
+				if (set.contains(currentPos)) {
+					Pair newPair = new Pair(i, currentPos);
+					stack.push(newPair);
 				}
 			}
 
