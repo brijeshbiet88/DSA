5da29a9247bb6be220f9877cb224aa506de697be
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-07-21 23:32:56 +0530

    Best Time to buy Sell Stock 4 Recursive. Leetcode 188

--- src/com/leetcode/dp/hard/BuySellStock4.java
+++ src/com/leetcode/dp/hard/BuySellStock4.java
@@ -3,8 +3,21 @@ package com.leetcode.dp.hard;
 public class BuySellStock4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("--------------Test Case 1-------------------");
+		int [] prices = {3,2,6,5,0,3};
+		int k = 2;
+		System.out.println("Max Profit = "+maxProfit(k, prices));
+		System.out.println();
+		
+		System.out.println("--------------Test Case 2-------------------");
+		System.out.println("Max Profit = "+maxProfitRec(k, prices));
+		System.out.println();
+		
+		System.out.println("--------------Test Case 3-------------------");
+		int [] prices2 = {3,3,5,0,0,3,1,4};
+		System.out.println("Max Profit = "+maxProfitRec(k, prices2));
+		System.out.println();
+		
 	}
 
 	 public static int maxProfit(int k, int[] prices) {
@@ -26,4 +39,35 @@ public class BuySellStock4 {
 
 			return dp[k][n - 1];
 	    }
+	 
+	 static Integer [][][] dp ;
+	 public static int maxProfitRec(int k, int[] prices) {
+	      if(prices == null || prices.length <= 1)
+	      return 0;
+	     
+	      dp = new Integer [prices.length+1][k+1][2];
+	      int buy = 1; 
+	      int tx = 0 , index = 0;
+	      
+	      return maxProfitRecHelper(prices , k , buy , tx  , index);
+	      
+	    }
+
+	 
+	private static int maxProfitRecHelper(int[] prices, int k, int buy, int tx , int i) {
+		
+		if(k <= tx || i == prices.length) {
+			return 0;
+		}
+		
+		if(dp[i][tx][buy] != null)
+			return dp[i][tx][buy];
+		
+		if(buy == 1) {
+			return dp[i][tx][buy] = Math.max(-prices[i] + maxProfitRecHelper(prices, k, 0,  tx , i+1), maxProfitRecHelper(prices, k, buy, tx , i+1));
+		}else {
+			return dp[i][tx][buy] = Math.max(prices[i] + maxProfitRecHelper(prices, k, 1,  tx+1 , i+1), maxProfitRecHelper(prices, k, 0, tx, i+1));
+		}
+	
+	}
 }
