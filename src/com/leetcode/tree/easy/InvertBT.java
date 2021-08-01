562c2a07f0a5bec5677d6f22f05c02b432255a00
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-08-01 22:50:29 +0530

    Invert Binary Tree Iterative. Leetcode 226

--- src/com/leetcode/tree/easy/InvertBT.java
+++ src/com/leetcode/tree/easy/InvertBT.java
@@ -1,5 +1,8 @@
 package com.leetcode.tree.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import com.leetcode.tree.TreeNode;
 import com.leetcode.tree.TreeUtil;
 
@@ -12,6 +15,9 @@ public class InvertBT {
 		root = invertTree(root);
 		System.out.println();
 		System.out.println("Level Order After Inverting BT ");
+		TreeUtil.traverseLevelByLevel(root);
+		root = invertTree2(root);
+		System.out.println("Level Order After Inverting BT Again ");
 		TreeUtil.traverseLevelByLevel(root);
 	}
 
@@ -29,6 +35,21 @@ public class InvertBT {
 		root.left = right;
 		root.right = temp;
 		return root;
+	}
+	
+	public static TreeNode invertTree2(TreeNode root) {
+	    if (root == null) return null;
+	    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+	    queue.add(root);
+	    while (!queue.isEmpty()) {
+	        TreeNode current = queue.poll();
+	        TreeNode temp = current.left;
+	        current.left = current.right;
+	        current.right = temp;
+	        if (current.left != null) queue.add(current.left);
+	        if (current.right != null) queue.add(current.right);
+	    }
+	    return root;
 	}
 
 }
