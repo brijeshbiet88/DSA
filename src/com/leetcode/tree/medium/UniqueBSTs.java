d194434b0a6f54c2c7c43018a0dbaf061902818b
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2021-08-12 14:03:47 +0530

    Unique Binary Search Trees. Leetcode 96

---
+++ src/com/leetcode/tree/medium/UniqueBSTs.java
@@ -0,0 +1,25 @@
+package com.leetcode.tree.medium;
+
+public class UniqueBSTs {
+
+	public static void main(String[] args) {
+		int n = 3;
+		System.out.println("No of Unique BSTs = "+numTrees(n));
+
+	}
+	
+	 public static int numTrees(int n) {
+	       int [] catalan = new int [n+1];
+	       
+	       catalan[0] = catalan[1] = 1;
+	       for(int i = 2 ; i <=n ; ++i) {
+	    	   catalan[i] = 0;
+	    	   for(int j = 0 ; j < i ; ++j) {
+	    		   catalan[i] += catalan[j]*catalan[i-j-1];
+	    	   }
+	       }
+	       
+	       return catalan[n];
+	 }
+
+}
