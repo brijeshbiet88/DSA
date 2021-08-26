9b7b1aff18ea5d16336361b844e77a1a460fb7e5
Brijesh <brijeshbiet88@gmail.com>
2021-08-26 10:05:20 +0530

    Daily Temperature. Leetcode 739

---
+++ src/com/leetcode/stack/medium/DailyTemperature.java
@@ -0,0 +1,44 @@
+package com.leetcode.stack.medium;
+
+import java.util.Stack;
+
+public class DailyTemperature {
+    public static void main(String[] args) {
+        System.out.println("-------------------Test Case 1---------------------");
+        int [] temperatures = {73,74,75,71,69,72,76,73};
+        int [] result = dailyTemperatures(temperatures);
+        System.out.println("Print Result ");
+        for(int days : result)
+            System.out.print(days+" ");
+        System.out.println();
+
+        System.out.println("-------------------Test Case 2---------------------");
+        int [] temperatures2 = {30,40,50,50 ,60};
+        int [] result2 = dailyTemperatures(temperatures2);
+        System.out.println("Print Result ");
+        for(int days : result2)
+            System.out.print(days+" ");
+        System.out.println();
+        
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] result = new int[len];
+
+        Stack<Integer> stack = new Stack<>();// Store Index of each day
+
+        for(int i = 0; i < len; i++) {
+
+            while(!stack.isEmpty() &&
+                    temperatures[stack.peek()] < temperatures[i] ) {
+
+                result[stack.peek()] = i - stack.pop();
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
