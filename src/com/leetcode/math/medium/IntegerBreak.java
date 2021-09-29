771a55f0d8c4701e30111f9b602cfaded065d1b1
Brijesh <brijeshbiet88@gmail.com>
2021-09-29 15:02:45 +0530

    Integer Break. Leetcode 343

--- src/com/leetcode/math/medium/IntegerBreak.java
+++ src/com/leetcode/math/medium/IntegerBreak.java
@@ -16,6 +16,16 @@ public class IntegerBreak {
         n = 12;
         System.out.println("Max Product = "+integerBreak(n));
         System.out.println();
+
+        System.out.println("----------------Test Case 4-----------------");
+        n = 13;
+        System.out.println("Max Product = "+integerBreak2(n));
+        System.out.println();
+
+        System.out.println("----------------Test Case 5-----------------");
+        n = 10;
+        System.out.println("Max Product = "+integerBreak2(n));
+        System.out.println();
     }
 
     public static int integerBreak(int n) {
@@ -39,5 +49,34 @@ public class IntegerBreak {
 
         }
         return ans;
+    }
+
+    public static int integerBreak2(int n) {
+        int[] dp = new int[n+1];
+
+        dp[0]=0;
+        dp[1]=0;
+        dp[2]=1;
+        if (n>2) {
+            dp[3]=2;
+        }
+
+        for (int i = 4; i <= n; i++) {
+            int greatestMax = -1;
+            for (int j = 2; j <= i / 2; j++) {
+                int n1 = j;
+                int n2 = i - j;
+
+                // for each number, get the max product n1 and n2 broken up
+                int maxProduct1 = dp[n1] > n1? dp[n1]:n1;
+                int maxProduct2 = dp[n2] > n2? dp[n2]:n2;
+                int max = maxProduct1 * maxProduct2;
+                if (max > greatestMax) {
+                    greatestMax = max;
+                }
+            }
+            dp[i] = greatestMax;
+        }
+        return dp[n];
     }
 }
