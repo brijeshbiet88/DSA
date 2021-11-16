29e5867e90d7fe2b24c6829458ef6b1910cefc7f
Brijesh <brijeshbiet88@gmail.com>
2021-11-16 20:42:22 +0530

    Convert String To Integer . LC 8

---
+++ src/com/leetcode/miscellaneous/StringToInteger.java
@@ -0,0 +1,66 @@
+package com.leetcode.miscellaneous;
+
+public class StringToInteger {
+    public static void main(String[] args) {
+        System.out.println("-------------Test Case 1----------------");
+        String s = " 342";
+        int result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+
+        System.out.println("-------------Test Case 2----------------");
+        s = " 0032";
+        result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+
+        System.out.println("-------------Test Case 3----------------");
+        s = "4193 with words";
+        result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+
+        System.out.println("-------------Test Case 4----------------");
+        s = "words and 987";
+        result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+
+        System.out.println("-------------Test Case 5----------------");
+        s = "-91283472332";
+        result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+
+        System.out.println("-------------Test Case 6----------------");
+        s = "2147483648";
+        result = myAtoi(s);
+        System.out.println("Integer of String "+s+" = "+result);
+        System.out.println();
+    }
+
+    public static int myAtoi(String str) {
+        int index = 0, sign = 1, total = 0, n = str.length();
+        while (index < n && str.charAt(index) == ' ')
+            index++;
+
+        if (index < n && (str.charAt(index) == '+' || str.charAt(index) == '-')) {
+            sign = str.charAt(index++) == '+' ? 1 : -1;
+        }
+
+        //3. Convert number and avoid overflow
+        while (index < n) {
+            int digit = str.charAt(index) - '0';
+            if (digit < 0 || digit > 9) break;
+
+            //check if total will be overflow after 10 times and add digit
+            if (total > Integer.MAX_VALUE / 10 || total == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < digit)
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+            total = 10 * total + digit;
+            index++;
+        }
+
+        return total * sign;
+    }
+}
