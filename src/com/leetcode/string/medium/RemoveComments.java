abe214cf4039d023010519c33bf85a8e2127b817
Brijesh <brijeshbiet88@gmail.com>
2021-11-21 23:19:19 +0530

    Remove Comments. LC 722

---
+++ src/com/leetcode/string/medium/RemoveComments.java
@@ -0,0 +1,41 @@
+package com.leetcode.string.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveComments {
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> removeComments(String[] source) {
+        boolean inBlock = false;
+        StringBuilder newline = new StringBuilder();
+        List<String> result = new ArrayList();
+        for (String line: source) {
+            int i = 0;
+            char[] chars = line.toCharArray();
+            if (!inBlock)
+                newline = new StringBuilder();
+
+            while (i < line.length()) {
+                if (!inBlock && i+1 < line.length() && chars[i] == '/' && chars[i+1] == '*') {
+                    inBlock = true;
+                    i++;
+                } else if (inBlock && i+1 < line.length() && chars[i] == '*' && chars[i+1] == '/') {
+                    inBlock = false;
+                    i++;
+                } else if (!inBlock && i+1 < line.length() && chars[i] == '/' && chars[i+1] == '/') {
+                    break;
+                } else if (!inBlock) {
+                    newline.append(chars[i]);
+                }
+                i++;
+            }
+            if (!inBlock && newline.length() > 0) {
+                result.add(new String(newline));
+            }
+        }
+        return result;
+    }
+}
