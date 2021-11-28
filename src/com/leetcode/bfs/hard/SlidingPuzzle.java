6f3361b663c6a84614c530da7154f53505905d79
Brijesh <brijeshbiet88@gmail.com>
2021-11-28 22:28:12 +0530

    Sliding Puzzle. LC 773

---
+++ src/com/leetcode/bfs/hard/SlidingPuzzle.java
@@ -0,0 +1,115 @@
+package com.leetcode.bfs.hard;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SlidingPuzzle {
+    // 3-Bit Binary representation of 123450
+    private static final int FINAL_STATE = 0b001010011100101000;
+    // Possible destination of zero. Here DIRS[0] represents the most significant
+    // 3-Bit number in the state. And, DIRS[5] represents the least significant
+    // 3-Bit number in the state.
+    private static final int[][] DIRS = { { 1, 3 }, { 0, 2, 4 }, { 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
+
+
+    public static void main(String[] args) {
+        System.out.println("----------------Test Case 1----------------------");
+        int [][] board1 = {{1,2,3},{4,0,5}};
+        System.out.println("Total Moves = "+slidingPuzzle(board1));
+        System.out.println();
+
+        System.out.println("----------------Test Case 2----------------------");
+        int [][] board2 = {{1,2,3},{5 ,4,0}};
+        System.out.println("Total Moves = "+slidingPuzzle(board2));
+        System.out.println();
+
+        System.out.println("----------------Test Case 3----------------------");
+        int [][] board3 = {{4, 1,2},{5,0,3}};
+        System.out.println("Total Moves = "+slidingPuzzle(board3));
+        System.out.println();
+
+        System.out.println("----------------Test Case 4----------------------");
+        int [][] board4 = {{3,2,4},{1,5,0}};
+        System.out.println("Total Moves = "+slidingPuzzle(board4));
+        System.out.println();
+    }
+
+
+    public static int slidingPuzzle(int[][] board) {
+        if (board == null || board.length != 2 || board[0].length != 3) {
+            throw new IllegalArgumentException("Input board is invalid");
+        }
+
+        int zeroIdx = -1;
+        int curState = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Inserting the num at the end of integer
+                curState = (curState << 3) | board[i][j];
+                if (board[i][j] == 0) {
+                    zeroIdx = i * 3 + j;
+                }
+            }
+        }
+
+        if (FINAL_STATE == curState) {
+            return 0;
+        }
+
+        HashSet<Integer> visited = new HashSet<>();
+        int moves = 0;
+
+        HashMap<Integer, Integer> begin = new HashMap<>();
+        begin.put(curState, zeroIdx);
+        visited.add(curState);
+
+        HashMap<Integer, Integer> end = new HashMap<>();
+        end.put(FINAL_STATE, 5);
+        visited.add(FINAL_STATE);
+
+        while (!begin.isEmpty()) {
+            if (begin.size() > end.size()) {
+                HashMap<Integer, Integer> tempSet = begin;
+                begin = end;
+                end = tempSet;
+            }
+
+            HashMap<Integer, Integer> next = new HashMap<>();
+            moves++;
+            for (int cur : begin.keySet()) {
+                zeroIdx = begin.get(cur);
+
+                for (int d : DIRS[zeroIdx]) {
+                    int newState = swap(cur, zeroIdx, d);
+                    if (end.containsKey(newState)) {
+                        return moves;
+                    }
+                    if (visited.add(newState)) {
+                        next.put(newState, d);
+                    }
+                }
+            }
+            begin = next;
+        }
+
+        return -1;
+    }
+
+    private static int swap(int state, int zeroIdx, int destIdx) {
+        // Generate mask for destIdx
+        int mask = 0b111 << ((5 - destIdx) * 3);
+        // Extracting num at destIdx
+        int num = state & mask;
+        // Moving num to zeroIdx
+        if (zeroIdx < destIdx) {
+            num <<= (destIdx - zeroIdx) * 3;
+        } else {
+            num >>>= (zeroIdx - destIdx) * 3;
+        }
+
+        // Setting zero at destIdx
+        state &= ~mask;
+        // Setting num at zeroIdx
+        return state | num;
+    }
+}
