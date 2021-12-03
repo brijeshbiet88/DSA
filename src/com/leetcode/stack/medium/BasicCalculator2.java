98960d6b7cf4ba5dd2cb81a07b9e570e7e8a3cd9
Brijesh <brijeshbiet88@gmail.com>
2021-12-03 19:48:56 +0530

    Basic Calculator 2. LC 227

---
+++ src/com/leetcode/stack/medium/BasicCalculator2.java
@@ -0,0 +1,74 @@
+package com.leetcode.stack.medium;
+
+import java.util.Stack;
+
+public class BasicCalculator2 {
+    public static void main(String[] args) {
+        System.out.println("----------Test Case 1---------------");
+        String s = "2 + 3 - 6 * 4";
+        System.out.println("Result = "+calculate(s));
+        System.out.println();
+
+        System.out.println("----------Test Case 2---------------");
+        s = "2+4/2-6*2/12 ";
+        System.out.println("Result = "+calculate(s));
+        System.out.println();
+
+        System.out.println("----------Test Case 3---------------");
+        s = "6/3*2+1";
+        System.out.println("Result = "+calculate(s));
+        System.out.println();
+
+        System.out.println("----------Test Case 4---------------");
+        s = "-2 + 8 + 4";
+        System.out.println("Result = "+calculate(s));
+        System.out.println();
+
+        System.out.println("----------Test Case 5---------------");
+        s = "1/2*6-3*1+90";
+        System.out.println("Result = "+calculate(s));
+        System.out.println();
+    }
+
+    public static int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        char prevSign = '+';
+        int no = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                no = no*10  + (c-'0');
+            }
+
+            if(isOperator(c) || i ==s.length()-1){
+                if(prevSign == '+'){
+                    stack.push(no);
+                } else if(prevSign == '-'){
+                    stack.push(-no);
+                } else if(prevSign =='*'){
+                    int top = stack.pop();
+
+                    stack.push(top * no);
+
+                } else if(prevSign =='/'){
+                    int top = stack.pop();
+                    stack.push(top/no);
+                }
+                no =0;
+                prevSign =c;
+            }
+        }
+
+        int res = 0;
+        while(!stack.empty()){
+            res += stack.pop();
+        }
+        return res;
+    }
+
+    private static boolean isOperator(char c){
+        return c=='+' || c=='-'|| c=='/' || c=='*';
+    }
+
+}
