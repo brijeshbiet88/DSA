cb2daf4749c62c2e8d67278371f7a4e4d60d5951
Brijesh <brijeshbiet88@gmail.com>
2021-12-25 09:29:28 +0530

    Basic Calculator without stack | LC 227

--- src/com/leetcode/stack/medium/BasicCalculator2.java
+++ src/com/leetcode/stack/medium/BasicCalculator2.java
@@ -21,12 +21,12 @@ public class BasicCalculator2 {
 
         System.out.println("----------Test Case 4---------------");
         s = "-2 + 8 + 4";
-        System.out.println("Result = "+calculate(s));
+        System.out.println("Result = "+calculate2(s));
         System.out.println();
 
         System.out.println("----------Test Case 5---------------");
         s = "1/2*6-3*1+90";
-        System.out.println("Result = "+calculate(s));
+        System.out.println("Result = "+calculate2(s));
         System.out.println();
     }
 
@@ -71,4 +71,30 @@ public class BasicCalculator2 {
         return c=='+' || c=='-'|| c=='/' || c=='*';
     }
 
+    public  static int calculate2(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if ("+-*/".indexOf(currentChar) != -1  || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
 }
