d24c60114c03fbe4dcc6c294b589c5385529dd8a
Brijesh <brijeshbiet88@gmail.com>
2022-01-07 22:01:13 +0530

    Observer Design Pattern

---
+++ src/com/designpattern/demo/Observer/Subject.java
@@ -0,0 +1,13 @@
+package com.designpattern.demo.Observer;
+
+public interface Subject {
+
+    public void register(Observer obj);
+    public void unregister(Observer obj);
+
+    public void notifyObservers();
+
+    //method to get updates from subject
+    public Object getUpdate(Observer obj);
+
+}
